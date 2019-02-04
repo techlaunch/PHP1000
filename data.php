f1377495592504099ac6101fe2449ea58aad66c0
Salvi Pascual <salvi.pascual@gmail.com>
2019-02-04 16:43:42 -0500

    Add files via upload

---
+++ data.php
@@ -0,0 +1,64 @@
+<?php
+
+$title = "Food from the world";
+
+$tags = ["Travel","New York","Dinner","Salmon","France","Drinks","Ideas","Flavors","Cuisine","Chicken","Dressing","Fried","Fish","Duck"];
+
+$food = [
+	[
+		"id" => "1",
+		"image" => "https://www.w3schools.com/w3images/sandwich.jpg",
+		"alt" => "Sandwich",
+		"title" => "The Perfect Sandwich, A Real NYC Classic",
+		"desc" => "Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum."
+	],
+	[
+		"id" => "2",
+		"image" => "https://www.w3schools.com/w3images/steak.jpg",
+		"alt" => "Steak",
+		"title" => "Let Me Tell You About This Steak",
+		"desc" => "Once again, some random text to lorem lorem lorem lorem ipsum text praesent tincidunt ipsum lipsum."
+	],
+	[
+		"id" => "3",
+		"image" => "https://www.w3schools.com/w3images/cherries.jpg",
+		"alt" => "Cherries",
+		"title" => "Cherries, interrupted",
+		"desc" => "Lorem ipsum text praesent tincidunt ipsum lipsum."
+	],
+	[
+		"id" => "4",
+		"image" => "https://www.w3schools.com/w3images/wine.jpg",
+		"alt" => "Pasta and Wine",
+		"title" => "Once Again, Robust Wine and Vegetable Pasta",
+		"desc" => "Lorem ipsum text praesent tincidunt ipsum lipsum."
+	],
+	[
+		"id" => "5",
+		"image" => "https://www.w3schools.com/w3images/popsicle.jpg",
+		"alt" => "Popsicle",
+		"title" => "All I Need Is a Popsicle",
+		"desc" => "Lorem ipsum text praesent tincidunt ipsum lipsum."
+	],
+	[
+		"id" => "6",
+		"image" => "https://www.w3schools.com/w3images/salmon.jpg",
+		"alt" => "Salmon",
+		"title" => "Salmon For Your Skin",
+		"desc" => "Once again, some random text to lorem lorem lorem lorem ipsum text praesent tincidunt ipsum lipsum."
+	],
+	[
+		"id" => "7",
+		"image" => "https://www.w3schools.com/w3images/sandwich.jpg",
+		"alt" => "Sandwich",
+		"title" => "The Perfect Sandwich, A Real Classic",
+		"desc" => "Just some random text, lorem ipsum text praesent tincidunt ipsum lipsum."
+	],
+	[
+		"id" => "8",
+		"image" => "https://www.w3schools.com/w3images/croissant.jpg",
+		"alt" => "Croissant",
+		"title" => "Le French",
+		"desc" => "Lorem lorem lorem lorem ipsum text praesent tincidunt ipsum lipsum."
+	]
+];
